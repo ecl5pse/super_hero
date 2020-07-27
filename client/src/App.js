e09d0b38a60741d04d4a2c212dcf9c4925b0ed7d
ecli5pse <fantamas20@gmail.com>
2020-07-27 19:48:27 +0300

    Create Client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Page from './pages';
+
+function App() {
+  return (
+    <div className="App">
+      <Page/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const Page = ()=>{
+  return null
+}
+
+export  default Page
